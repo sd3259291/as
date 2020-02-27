c13c2e37a41531bf07b1e5aa2f3f609b33515872
aya <42686304@qq.com>
2020-02-27 13:54:52 +0800

    push

--- tp/config/view.php
+++ tp/config/view.php
@@ -22,4 +22,8 @@ return [
     'taglib_begin'  => '{',
     // 标签库标签结束标记
     'taglib_end'    => '}',
+	'tpl_replace_string'  =>  [
+		'__PUBLIC__' => dirname($_SERVER['SCRIPT_NAME']).'/tp/view/public'
+	]
+
 ];
